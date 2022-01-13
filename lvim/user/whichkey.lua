20e2299e0ab97b0e90afbcce503002913e5ef71c
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-01-13 16:27:21 +0000

    add keymappings for sessions

--- lvim/user/whichkey.lua
+++ lvim/user/whichkey.lua
@@ -30,5 +30,7 @@ lvim.builtin.which_key.mappings["S"] = {
 	name = "Session",
 	c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
 	l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+	s = { "<cmd>lua require('persistence').save()<cr>", "Save session" },
+	a = { "<cmd>lua require('persistence').start()<cr>", "Automatically save when leave" },
 	Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
