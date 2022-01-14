8f97debf3ee7d28e53fa18849bec4e2ab06b5f35
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-01-14 12:23:22 +0000

    clean file

--- lvim/user/keymaps.lua
+++ lvim/user/keymaps.lua
@@ -1,10 +1,7 @@
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
+
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = false
--- edit a default keymapping
 lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- navigation within insert mode
