6f4e0eeaa19e5dfec2c9ee36c0128467b358bbb4
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-01-14 12:21:57 +0000

    add navigation within insert mode

--- lvim/user/keymaps.lua
+++ lvim/user/keymaps.lua
@@ -6,3 +6,11 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.keys.normal_mode["<C-Up>"] = false
 -- edit a default keymapping
 lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- navigation within insert mode
+lvim.keys.insert_mode["<C-h>"] = "<Left>"
+lvim.keys.insert_mode["<C-l>"] = "<Right>"
+lvim.keys.insert_mode["<C-k>"] = "<Up>"
+lvim.keys.insert_mode["<C-j>"] = "<Down>"
+lvim.keys.insert_mode["<C-e>"] = "<End>"
+lvim.keys.insert_mode["<C-a>"] = "<Esc>^i"
