d53b150662a75a9660896050726d872a20478b03
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-01-16 23:08:55 +0800

    config(lvim) disable jj and enable ts-rainbow

--- lvim/user/keymaps.lua
+++ lvim/user/keymaps.lua
@@ -12,6 +12,7 @@ lvim.keys.insert_mode["<C-k>"] = "<Up>"
 lvim.keys.insert_mode["<C-j>"] = "<Down>"
 lvim.keys.insert_mode["<C-e>"] = "<End>"
 lvim.keys.insert_mode["<C-a>"] = "<Esc>^i"
+lvim.keys.insert_mode["jj"] = false
 
 -- visual mode
 lvim.keys.visual_mode["p"] = '"_dP'
