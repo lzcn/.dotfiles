54891c01dcb3419720c4b0e3cfadfdb44428de1b
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-02-13 23:01:18 +0800

    config(lvim): update configuration

--- lvim/config.lua
+++ lvim/config.lua
@@ -9,8 +9,10 @@ require("lv-user-config.lualine")
 
 -- toggleterm
 lvim.builtin.terminal.active = true
--- toggleterm configurations
-require("lv-user-config.terminal")
+lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs + 1] = { "ipython", "<c-\\><c-p>", "iPython" }
+lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs + 1] = { "node", "<c-\\><c-n>", "node" }
+lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs + 1] = { "htop", "<c-\\><c-h>", "htop" }
+lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs + 1] = { "ncdu", "<c-\\><c-u>", "ncdu" }
 
 -- keymappings
 require("lv-user-config.keymaps")
