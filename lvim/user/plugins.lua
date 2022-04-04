e0b3ad5e9fde6dfcc832782277e422dfbb0befad
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-04-04 20:18:18 +0800

    refactor(lvim): reorganize plugins

--- lvim/user/plugins.lua
+++ lvim/user/plugins.lua
@@ -1,14 +1,65 @@
 lvim.plugins = {
-	-- for colorscheme
-	{ "folke/tokyonight.nvim" },
-	{ "joshdick/onedark.vim" },
+	-- Aesthetics --
+	-- colorscheme
+	-- { "folke/tokyonight.nvim" },
+	-- { "joshdick/onedark.vim" },
 	{ "Mofiqul/dracula.nvim" },
+	-- rainbow parentheses
 	{ "p00f/nvim-ts-rainbow" },
+	-- display color
+	{ "norcalli/nvim-colorizer.lua" },
+
+	-- LSP --
+	-- diagnostics highlight for non-LSP colorscheme
+	{ "folke/lsp-colors.nvim" },
+	-- previewing definition
 	{
-		"folke/lsp-colors.nvim",
+		"rmagatti/goto-preview",
+		config = function()
+			require("goto-preview").setup({})
+		end,
+	},
+	-- hitn signature when type
+	{
+		"ray-x/lsp_signature.nvim",
 		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup()
+		end,
 	},
 
+	-- Diagnostics --
+	{ "folke/trouble.nvim", cmd = "TroubleToggle" },
+	-- better quickfix window
+	{ "kevinhwang91/nvim-bqf", ft = "qf" },
+
+	-- Navigation --
+	-- enhanced matchup
+	{ "andymass/vim-matchup" },
+	-- navigate within tmux panes
+	{
+		"numToStr/Navigator.nvim",
+		config = function()
+			require("Navigator").setup()
+		end,
+	},
+	-- peek line
+	{
+		"nacro90/numb.nvim",
+		config = function()
+			require("numb").setup()
+		end,
+	},
+	-- open projects with telescope
+	{
+		"nvim-telescope/telescope-project.nvim",
+		event = "BufWinEnter",
+		setup = function()
+			vim.cmd([[packadd telescope.nvim]])
+		end,
+	},
+
+	-- Python --
 	-- pydocstring
 	{
 		"heavenshell/vim-pydocstring",
@@ -18,8 +69,12 @@ lvim.plugins = {
 			vim.g.pydocstring_formatter = "google"
 		end,
 	},
+	-- jupyter-notebook
+	{ "dccsillag/magma-nvim", run = ":UpdateRemotePlugins", ft = { "python" } },
+	-- sphinx support
+	{ "stsewd/sphinx.nvim", run = ":UpdateRemotePlugins", ft = { "rst" } },
 
-	-- latex
+	-- LaTeX --
 	{
 		"lervag/vimtex",
 		ft = { "tex" },
@@ -32,17 +87,20 @@ lvim.plugins = {
 		end,
 	},
 
-	-- jupyter
-	{ "dccsillag/magma-nvim", run = ":UpdateRemotePlugins", ft = { "python" } },
+	-- Markdown --
+	-- preview with glow
+	{ "npxbr/glow.nvim", ft = { "markdown" } },
+	-- preview on browser
+	{
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		ft = "markdown",
+		config = function()
+			vim.g.mkdp_auto_start = 1
+		end,
+	},
 
-	-- navigate within tmux
-	-- {
-	-- 	"numToStr/Navigator.nvim",
-	-- 	config = function()
-	-- 		require("Navigator").setup()
-	-- 	end,
-	-- },
-
+	-- Utility --
 	-- copilot
 	{
 		"github/copilot.vim",
@@ -50,37 +108,6 @@ lvim.plugins = {
 			vim.g.copilot_no_tab_map = true
 		end,
 	},
-
-	-- diagnostics
-	{ "folke/trouble.nvim", cmd = "TroubleToggle" },
-
-	{
-		"nvim-telescope/telescope-project.nvim",
-		event = "BufWinEnter",
-		setup = function()
-			vim.cmd([[packadd telescope.nvim]])
-		end,
-	},
-
-	-- better navigation
-	{ "andymass/vim-matchup" },
-	{ "kevinhwang91/nvim-bqf", ft = "qf" },
-	-- peeks lines
-	{
-		"nacro90/numb.nvim",
-		config = function()
-			require("numb").setup()
-		end,
-	},
-
-	-- smooth scroll
-	-- {
-	-- 	"karb94/neoscroll.nvim",
-	-- 	event = "WinScrolled",
-	-- 	config = function()
-	-- 		require("neoscroll").setup()
-	-- 	end,
-	-- },
 
 	-- automatic session saver
 	{
@@ -110,37 +137,6 @@ lvim.plugins = {
 		end,
 	},
 
-	-- color display
-	{
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup({ "*" }, {
-				RGB = true, -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				rgb_fn = true, -- CSS rgb() and rgba() functions
-				hsl_fn = true, -- CSS hsl() and hsla() functions
-				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-				mode = "background", -- Set the display mode
-			})
-		end,
-	},
-
-	-- sphinx
-	{ "stsewd/sphinx.nvim", ft = { "rst" } },
-
-	-- markdown preview
-	{ "npxbr/glow.nvim", ft = { "markdown" } },
-
-	{
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		ft = "markdown",
-		config = function()
-			vim.g.mkdp_auto_start = 1
-		end,
-	},
 	-- todo comments
 	{
 		"folke/todo-comments.nvim",
@@ -150,48 +146,15 @@ lvim.plugins = {
 		end,
 	},
 
-	-- mappings to delete, change and add surroundings
+	-- delete, change and add surroundings
 	{
 		"tpope/vim-surround",
 		keys = { "c", "d", "y" },
 	},
 
-	-- underlines the word under the cursor
+	-- highlight the word under the cursor
 	{
-		"itchyny/vim-cursorword",
+		"xiyaowong/nvim-cursorword",
 		event = { "BufEnter", "BufNewFile" },
-		config = function()
-			vim.api.nvim_command("augroup user_plugin_cursorword")
-			vim.api.nvim_command("autocmd!")
-			vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
-			vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
-			vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
-			vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
-			vim.api.nvim_command("augroup END")
-		end,
-	},
-
-	-- previewing goto definition calls
-	{
-		"rmagatti/goto-preview",
-		config = function()
-			require("goto-preview").setup({
-				width = 120, -- Width of the floating window
-				height = 25, -- Height of the floating window
-				default_mappings = false, -- Bind default mappings
-				debug = false, -- Print debug information
-				opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
-				post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
-			})
-		end,
-	},
-
-	-- hint signature when you type
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "BufRead",
-		config = function()
-			require("lsp_signature").setup()
-		end,
 	},
 }
