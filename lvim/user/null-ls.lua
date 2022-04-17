e717fdd8d7a5572519c2de3386b3c27bfddd1056
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-04-17 19:00:28 +0800

    config(lvim): update null-ls and lualine
    1. set eslinter and prettier for TS and JS
    2. use globalstatus of lualine (neovim 0.7 and higher)

--- lvim/user/null-ls.lua
+++ lvim/user/null-ls.lua
@@ -3,11 +3,12 @@ formatters.setup({
 	{ command = "isort", filetypes = { "python" } },
 	{ command = "black", filetypes = { "python" } },
 	{ command = "stylua", filetypes = { "lua" } },
-	{ command = "prettier" },
+	{ command = "prettier", filetypes = { "typescript", "javascript" } },
 })
 
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
+	{ command = "eslint", filetypes = { "typescript", "javascript" } },
 	{ command = "flake8", filetypes = { "python" } },
 	{ command = "shellcheck", extra_args = { "--severity", "warning" } },
 	{ command = "codespell" },
