47531154179f716778e9e1ad939302e6dadf5034
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-05-28 15:52:36 +0800

    lvim: hightlight zsh as bash

--- lvim/config.lua
+++ lvim/config.lua
@@ -100,3 +100,12 @@ lvim.lsp.diagnostics.virtual_text = false
 
 -- null-ls configurations
 require("lv-user-config.null-ls")
+
+-- autocommands
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "zsh",
+	callback = function()
+		-- let treesitter use bash highlight for zsh files as well
+		require("nvim-treesitter.highlight").attach(0, "bash")
+	end,
+})
