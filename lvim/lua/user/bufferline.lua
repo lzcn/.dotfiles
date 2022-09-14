bcdecc103a7a4dfccb5c0910b57cf5a9dc700990
Zhi Lu <zhilu@std.uestc.edu.cn>
2022-09-15 00:14:04 +0800

    clean(lvim): bufferline

--- lvim/lua/user/bufferline.lua
+++ lvim/lua/user/bufferline.lua
@@ -1,4 +1,2 @@
 lvim.builtin.bufferline.active = true
-lvim.builtin.bufferline.options.indicator_icon = nil
-lvim.builtin.bufferline.options.indicator = { style = "icon", icon = "▎" }
 lvim.builtin.bufferline.options.always_show_bufferline = true
