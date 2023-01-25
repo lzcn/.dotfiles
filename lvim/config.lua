fe725ffbb8bac33f90a24bedf104162e1974da0c
Zhi Lu <zhilu@std.uestc.edu.cn>
2023-01-25 13:13:41 +0800

    lvim(style): change theme to tokyonight, cleanup

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,26 +1,5 @@
--- vim options
-vim.opt.scrolloff = 5
-vim.opt.relativenumber = true
--- vim.opt.termguicolors = true
-
--- colorscheme
-lvim.colorscheme = "dracula"
-
--- user configurations
-lvim.user = {
-  copilot = { active = true, cmp = true },
-  csv = { active = true },
-  fcitx = { active = false },
-  indentline = { active = false },
-  lastplace = { active = true },
-  log = { active = true },
-  lsp = { timeout_ms = 3000 },
-  lsp_signature = { active = false },
-  macos = vim.fn.has("macunix") > 0,
-  markdown = { glow = false, preview = true },
-  navigation = { lightspeed = false, numb = true, rnvimr = false },
-  tex = { active = true },
-}
+-- options
+require("user.options")
 
 -- plugins
 require("user.plugins")
