4337f3ff2a8c465dfc3960d8667d37a38ebc2759
Zhi Lu <zhilu@ustc.edu.cn>
2023-04-26 20:10:37 +0800

    lvim: use pylsp

--- lvim/lua/user/lsp.lua
+++ lvim/lua/user/lsp.lua
@@ -1,6 +1,11 @@
-lvim.lsp.installer.setup.ensure_installed = {
-  "lua_ls",
-  "jsonls",
-  "pyright",
-}
+-- lvim.lsp.installer.setup.ensure_installed = {
+--   "lua_ls",
+--   "jsonls",
+--   "pyright",
+-- }
 lvim.lsp.diagnostics.virtual_text = false
+-- use pylsp instead of pyright
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pylyzer", "pyright" })
+lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+  return server ~= "pylsp"
+end, lvim.lsp.automatic_configuration.skipped_servers)
