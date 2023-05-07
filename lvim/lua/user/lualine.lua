8da4b326f536510a29ede40a26cc31862ffed2f9
Zhi Lu <zhilu@ustc.edu.cn>
2023-05-07 15:58:55 +0800

    update lvim configs
    better lualine
    disable folding by treesitter

--- lvim/lua/user/lualine.lua
+++ lvim/lua/user/lualine.lua
@@ -1,1 +1,11 @@
+local components = require "lvim.core.lualine.components"
+
+lvim.builtin.lualine.style = "lvim"
+lvim.builtin.lualine.options.theme = lvim.colorscheme
+lvim.builtin.lualine.sections.lualine_a = { "mode" }
+lvim.builtin.lualine.sections.lualine_c = {
+  components.diff,
+  components.python_env,
+  "filename",
+}
 lvim.builtin.lualine.options.globalstatus = true
