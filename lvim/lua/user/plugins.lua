c617fbe4de9305e3ccf0555f0f89ab7cd9e7296c
Zhi Lu <zhilu@ustc.edu.cn>
2023-07-08 15:53:05 +0800

    clean

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -319,16 +319,4 @@ lvim.plugins = {
       require("registers").setup { show_empty = false }
     end,
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    config = function()
-      require("chatgpt").setup {}
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    enabled = os.getenv "OPENAI_API_KEY" ~= nil,
-  },
 }
