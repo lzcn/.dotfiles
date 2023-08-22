aec4c245d40e21d0bc16b05d1a2807e0d64c5928
Zhi Lu <zhilu@ustc.edu.cn>
2023-08-22 17:27:12 +0800

    lvim: disable linter

--- lvim/lua/user/null-ls.lua
+++ lvim/lua/user/null-ls.lua
@@ -9,8 +9,8 @@ formatters.setup {
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { command = "eslint", filetypes = { "typescript", "javascript" } },
-  { command = "flake8", filetypes = { "python" } },
+  -- { command = "eslint", filetypes = { "typescript", "javascript" } },
+  -- { command = "flake8", filetypes = { "python" } },
   { command = "shellcheck", extra_args = { "--severity", "warning" } },
   { command = "codespell" },
 }
