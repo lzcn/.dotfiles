c177fd782f26bf82f597155c2536e96e0aa1f520
Zhi Lu <zhilu@ustc.edu.cn>
2024-02-29 15:33:08 +0800

    lvim: change dap icons

--- lvim/lua/user/dap.lua
+++ lvim/lua/user/dap.lua
@@ -1,5 +1,11 @@
 -- debug adapter protocol
 lvim.builtin.dap.active = true
+lvim.builtin.dap.breakpoint.text = "●"
+lvim.builtin.dap.stopped.text = "▶"
+
+vim.cmd [[highlight DiagnosticSignError guifg=#F44747]]
+vim.cmd [[highlight DiagnosticSignWarn guifg=#FFCC00]]
+
 -- support configurations using 'launch.json' file
 lvim.builtin.dap.on_config_done = function()
   require("dap.ext.vscode").load_launchjs()
