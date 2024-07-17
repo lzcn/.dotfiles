b66181390408375681b90091b53e908ce6dbb4e6
Zhi Lu <zhilu@ustc.edu.cn>
2024-07-17 22:51:00 +0800

    nvim: remove some extra configurations

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -8,20 +8,6 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
-    keys = {
-      -- disable the keymap to grep files
-      { "<leader>/", false },
-      -- change a keymap
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      -- add a keymap to browse plugin files
-      {
-        "<leader>fp",
-        function()
-          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-        end,
-        desc = "Find Plugin File",
-      },
-    },
   },
   -- {
   --   "folke/which-key.nvim",
@@ -29,6 +15,20 @@ return {
   --   opts = {
   --     defaults = {
   --       ["<leader>w"] = { "<cmd>update<cr>", "Save" },
+  --     },
+  --   },
+  --   keys = {
+  --     -- disable the keymap to grep files
+  --     { "<leader>/", false },
+  --     -- change a keymap
+  --     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+  --     -- add a keymap to browse plugin files
+  --     {
+  --       "<leader>fp",
+  --       function()
+  --         require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+  --       end,
+  --       desc = "Find Plugin File",
   --     },
   --   },
   -- },
